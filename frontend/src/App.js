9fd5b5004ad83c0054217ec3d0cf88490b30c556
Your New Name <yournewemail@example.com>
2026-02-09 23:47:10 +0530

    test prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="flex h-screen items-center justify-center bg-blue-100">
       <div className="p-10 bg-white rounded-lg shadow-xl">
-        <h1 className="        text-4xl font-bold text-blue-600">
+        <h1 className="text-4xl font-bold text-blue-600">
           Flux Energy Audit
         </h1>
       </div>
