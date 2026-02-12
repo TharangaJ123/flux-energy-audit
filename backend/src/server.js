d2b308f998d656ba2807cb801c28b8049cf48130
VenuriN <nuwangawatagedar@gmail.com>
2026-02-13 00:25:48 +0530

    commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const dns = require('dns');
+
+// Set DNS servers to resolve MongoDB SRV records
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const connectDB = require('./config/db');
+const { swaggerUi, specs } = require('./config/swagger');
 
 // Load env vars
 dotenv.config();
@@ -16,8 +22,12 @@ app.use(express.json());
 app.use(cors());
 
 // Define Routes
+app.get('/', (req, res) => {
+  res.send('Flux Energy Audit API is running...');
+});
 
 // Swagger Documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 const PORT = process.env.PORT || 5000;
 
