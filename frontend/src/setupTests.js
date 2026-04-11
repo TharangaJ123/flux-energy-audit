c4c727f2c48efd54692818e020cf33fea6fac7f5
Tharanga Jayawardhana <chathuratharanga076@gmail.com>
2026-04-11 20:01:27 +0530

    frontend unit tests for SolarEstimator, CostManagement, and EnergyAuditManagement pages with global test configuration.

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -4,6 +4,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 jest.mock('react-router-dom', () => require('./test-utils/reactRouterDomMock'), { virtual: true });
 
